bfdaa511983f2016dcd5d5e9997f5e4715e428da
Alejandro Desatnik Sod <Alejandro@wireless-s1-no-150-17-11.uchicago.edu>
2015-10-11 23:36:21 -0500

    lalala

--- degreeplanner/urls.py
+++ degreeplanner/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from registration.backends.simple.views import RegistrationView
 
 # Create a new class that redirects the user to the index page, if successful at logging
-class MyRegistrationView(RegistrationView):
+
+"""class MyRegistrationView(RegistrationView):
     def get_success_url(self,request, user):
-        return '/planner/'
+        return '/planner/'"""
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
