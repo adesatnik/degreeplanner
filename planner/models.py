419ed03b1c43db6dd4fc294cb9219ecdeb293eff
Alejandro Desatnik Sod <Alejandro@wireless-s1-no-150-17-11.uchicago.edu>
2015-10-13 02:33:48 -0500

    Created scraping models

--- planner/models.py
+++ planner/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from dynamic_scraper.models import Scraper, SchedulerRuntime
+from scrapy.contrib.djangoitem import DjangoItem
 
 # Create your models here.
 
@@ -8,23 +10,23 @@ class Course(models.Model):
     code = models.CharField(max_length=150 )
     name = models.CharField(max_length=250)
     department = models.CharField(max_length=150)
-    
+
     def __unicode__(self):
         return self.code
 
-    
+
 class DegreePlan(models.Model):
     name = models.CharField(max_length = 250)
     owner = models.ForeignKey(User)
     slug = models.SlugField(unique = True)
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(str(self.name) + str(self.owner))
         super(DegreePlan, self).save(*args, **kwargs)
-    
+
     def __unicode__(self):
         return self.name
-  
+
 
 
 class Class(models.Model):
@@ -38,7 +40,28 @@ class Class(models.Model):
     year = models.IntegerField()
     quarter = models.CharField(max_length=50, choices=QUARTERS)
     taken = models.BooleanField(default = False)
-    
-    
+
+
     def __unicode__(self):
         return self.course.code
+
+
+class ClassWebsite(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    scraper = models.ForeignKey(Scraper, blank=True, null=True, on_delete=models.SET_NULL)
+    scraper_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __unicode__(self):
+        return self.name
+
+class ScrClass (models.model):
+    code = models.CharField(max_length=150 )
+    name = models.CharField(max_length=250)
+    department = models.CharField(max_length=150)
+    checker_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+    website = models.ForeignKey(ClassWebsite)
+
+
+class ScrClassItem(DjangoItem):
+    django_model = ScrClass
